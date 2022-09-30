2408bb1878137b64f1e450cbe9100d6d63233532
Nex Sabre <NexSabre@users.noreply.github.com>
2022-09-30 10:06:22 +0200

    Correct lint

--- gosharkapi/api.go
+++ gosharkapi/api.go
@@ -1,35 +1,41 @@
 package gosharkapi
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/PacketHelper/goshark/v2/goshark"
 	"github.com/gin-gonic/gin"
 )
 
+func statusZ(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
+func getHex(ctx *gin.Context) {
+	hexValue := ctx.Param("hex")
+
+	hexArray, err := goshark.Hex2Array(hexValue)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "wrong packet",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"hex": hexArray,
+	})
+}
+
 func HttpServer() {
 	r := gin.Default()
-	r.GET(
-		"/status", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "ok",
-			})
-		})
+	r.GET("/statusz", statusZ)
 
-	r.GET(
-		"/api/v1/hex/:hex", func(ctx *gin.Context) {
-			hexValue := ctx.Param("hex")
-
-			hexArray, err := goshark.Hex2Array(hexValue)
-			if err != nil {
-				ctx.JSON(http.StatusBadRequest, gin.H{
-					"error": "wrong packet",
-				})
-				return
-			}
-			ctx.JSON(http.StatusOK, gin.H{
-				"hex": hexArray,
-			})
-		})
-	r.Run()
+	r.GET("/api/v1/hex/:hex", getHex)
+	err := r.Run()
+	if err != nil {
+		log.Fatalf("cannot start http server %s", err)
+	}
 }
