de44056ca5517e0410e3a9a1acd48b2cc0add9b8
Nex Sabre <NexSabre@users.noreply.github.com>
2022-09-30 09:11:56 +0200

    Add /api/v1/hex/:hex

--- gosharkapi/api.go
+++ gosharkapi/api.go
@@ -3,6 +3,7 @@ package gosharkapi
 import (
 	"net/http"
 
+	"github.com/PacketHelper/goshark/v2/goshark"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,22 @@ func HttpServer() {
 		"/status", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"message": "ok",
+			})
+		})
+
+	r.GET(
+		"/api/v1/hex/:hex", func(ctx *gin.Context) {
+			hexValue := ctx.Param("hex")
+
+			hexArray, err := goshark.Hex2Array(hexValue)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"error": "wrong packet",
+				})
+				return
+			}
+			ctx.JSON(http.StatusOK, gin.H{
+				"hex": hexArray,
 			})
 		})
 	r.Run()
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/PacketHelper/goshark/v2/gosharkapi"
 )
 
 func main() {
+	fmt.Print("Starting goshark api...")
 	gosharkapi.HttpServer()
 }
