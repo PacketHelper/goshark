a3b0c0a8a1b2eb175a66564d5e85242223aaeefd
Nex Sabre <NexSabre@users.noreply.github.com>
2022-10-04 20:43:22 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 make cover
-curl -Os https://uploader.codecov.io/latest/linux/codecov
+curl -Os https://uploader.codecov.io/latest/macos/codecov
 chmod 777 codecov
 
-./codecov -t ${CODECOV}
+echo ${CODECOV}
+./codecov -t 339562ba-7a26-4b07-9dea-9f1aef74eefe
